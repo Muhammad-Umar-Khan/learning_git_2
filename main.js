63cad387955fec4fd37870e14960bd26b458c6e8
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-03 08:03:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const h1 = document.getElementById("h1");
+const p = document.getElementById("p");
+
+h1.innerText = "Cat";
+p.innerText = "The cat (Felis catus) is a domestic species of small carnivorous mammal.[1][2] It is the only domesticated species in the family Felidae and is commonly referred to as the domestic cat or house cat to distinguish it from the wild members of the family."
